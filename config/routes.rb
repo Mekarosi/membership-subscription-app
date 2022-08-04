b325988bc6ceaaa0eb21d83e7b9228472649cf26
Mekarosi <mekarosi_assa@yahoo.com>
2022-08-04 00:15:36 -0700

    Cleaned up contact form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get 'about', to: 'pages#about'
-  resources :communicates
-  get 'communicate-with-us', to: 'communicates#new'
+  resources :communicates, only: :create
+  get 'communicate-with-us', to: 'communicates#new', as: 'new_communicate'
 
   # resources :contacts
 end
