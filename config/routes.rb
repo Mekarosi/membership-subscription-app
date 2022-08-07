e4105218dbaf71b26547b1befdd11b54641eb94b
Mekarosi <mekarosi_assa@yahoo.com>
2022-08-07 01:10:20 -0700

    Added conditional logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
   root to: "pages#home"
   get 'about', to: 'pages#about'
   resources :communicates, only: :create
